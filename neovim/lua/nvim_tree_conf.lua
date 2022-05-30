971637b5b7faffe809cb4b01b63979c3c855b6af
zambelz48 <nanda.julianda48@gmail.com>
2022-05-30 09:24:19 +0700

    Update nvim tree configurations

--- neovim/lua/nvim_tree_conf.lua
+++ neovim/lua/nvim_tree_conf.lua
@@ -1,107 +1,111 @@
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_icon_padding = ' ' 
-vim.g.nvim_tree_icons = {
-	default = '',
-    symlink = '',
-    git = {
-      unstaged = "",
-      staged = "✓",
-      unmerged = "",
-      renamed = "➜",
-      untracked = "★",
-      deleted = "✗",
-      ignored = "◌"
-    },
-    folder = {
-      arrow_open = "",
-      arrow_closed = "",
-      default = "",
-      open = "",
-      empty = "",
-      empty_open = "",
-      symlink = "",
-      symlink_open = "",
-    }
-}
-
 -- keymap
 vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>:NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 require'nvim-tree'.setup {
-  -- disables netrw completely
-  disable_netrw       = true,
-  -- hijack netrw window on startup
-  hijack_netrw        = true,
-  -- open the tree when running this setup function
-  open_on_setup       = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when you run `:cd` usually)
-  update_cwd          = false,
-  -- show lsp diagnostics in the signcolumn
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = true,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {}
-  },
-  -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
-    -- the command to run this, leaving nil should work in most cases
-    cmd  = nil,
-    -- the command arguments as a list
-    args = {}
-  },
+	-- disables netrw completely
+	disable_netrw       = true,
+	-- hijack netrw window on startup
+	hijack_netrw        = true,
+	-- open the tree when running this setup function
+	open_on_setup       = false,
+	-- will not open on setup if the filetype is in this list
+	ignore_ft_on_setup  = {},
+	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+	open_on_tab         = false,
+	-- hijack the cursor in the tree to put it at the start of the filename
+	hijack_cursor       = false,
+	-- updates the root directory of the tree on `DirChanged` (when you run `:cd` usually)
+	update_cwd          = false,
+	-- show lsp diagnostics in the signcolumn
+	diagnostics = {
+		enable = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		}
+	},
+	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+	update_focused_file = {
+		-- enables the feature
+		enable      = true,
+		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+		-- only relevant when `update_focused_file.enable` is true
+		update_cwd  = true,
+		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+		ignore_list = {}
+	},
+	-- configuration options for the system open command (`s` in the tree by default)
+	system_open = {
+		-- the command to run this, leaving nil should work in most cases
+		cmd  = nil,
+		-- the command arguments as a list
+		args = {}
+	},
 
-  view = {
-    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-    width = 40,
-    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
-    height = 30,
-    -- Hide the root path of the current folder on top of the tree 
-    hide_root_folder = false,
-    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-    side = 'left',
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {}
-    }
-  },
+	view = {
+		-- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+		width = 40,
+		-- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+		height = 30,
+		-- Hide the root path of the current folder on top of the tree 
+		hide_root_folder = false,
+		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+		side = 'left',
+		mappings = {
+			-- custom only false will merge the list with the default mappings
+			-- if true, it will only use your list to set the mappings
+			custom_only = false,
+			-- list of mappings to set on the tree manually
+			list = {}
+		}
+	},
 
-  git = {
-    ignore = false
-  },
+	git = {
+		ignore = false
+	},
 
-  filters = {
-	dotfiles = false,
-    custom = {}
-  },
+	filters = {
+		dotfiles = false,
+		custom = {}
+	},
 
-  actions = {
-    open_file = {
-      quit_on_open = true
-    }
-  }
+	actions = {
+		open_file = {
+			quit_on_open = true
+		}
+	},
+
+	renderer = {
+		highlight_opened_files = "icon",
+		icons = {
+			padding = ' ',
+			glyphs = {
+				default = '',
+				symlink = '',
+				git = {
+					unstaged = "",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "✗",
+					ignored = "◌"
+				},
+				folder = {
+					arrow_open = "",
+					arrow_closed = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				}
+			}
+		}
+	}
 }
 
