7fd39829b64051c1e0d3b7bdf7db0c022cfbdee4
zambelz48 <nanda.julianda48@gmail.com>
2023-05-25 22:11:55 +0700

    Add more keymap in fugitive config

--- neovim/lua/fugitive_conf.lua
+++ neovim/lua/fugitive_conf.lua
@@ -3,6 +3,9 @@ local utils = require('utils')
 
 -- keymap
 utils.nnoremap('<leader>gs', ':G<CR>', { silent = true })
+utils.nnoremap('<leader>gl', ':G log<CR>', { silent = true })
+utils.nnoremap('<leader>glo', ':G log --oneline<CR>', { silent = true })
+utils.nnoremap('<leader>ga', ':Gwrite<CR>', { silent = true })
 utils.nnoremap('gds', ':Gdiffsplit<CR>', { silent = true })
 utils.nnoremap('<leader>gp', ':G push<CR>', { silent = true })
 utils.nnoremap('<leader>gpo', ':G push origin ')
