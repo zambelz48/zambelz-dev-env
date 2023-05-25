c54c3295b2a626e23c8938a5819fbf8719ac14d1
zambelz48 <nanda.julianda48@gmail.com>
2023-05-25 21:25:28 +0700

    used shared mapping function in toggleterm conf

--- neovim/lua/toggleterm_conf.lua
+++ neovim/lua/toggleterm_conf.lua
@@ -1,3 +1,5 @@
+local utils = require 'utils'
+
 require('toggleterm').setup({
 	size = 20,
 	open_mapping = [[<c-\>]],
@@ -22,13 +24,12 @@ require('toggleterm').setup({
 })
 
 function _G.set_terminal_keymaps()
-	local opts = { noremap = true }
-	vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-	vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+	utils.tnoremap_buf('<esc>', [[<C-\><C-n>]])
+	utils.tnoremap_buf('jk', [[<C-\><C-n>]])
+	utils.tnoremap_buf('<C-h>', [[<C-\><C-n><C-W>h]])
+	utils.tnoremap_buf('<C-j>', [[<C-\><C-n><C-W>j]])
+	utils.tnoremap_buf('<C-k>', [[<C-\><C-n><C-W>k]])
+	utils.tnoremap_buf('<C-l>', [[<C-\><C-n><C-W>l]])
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
