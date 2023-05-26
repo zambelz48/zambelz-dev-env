57a5a32eaff65afd7cc912a24cea0f21d2b8f7d6
zambelz48 <nanda.julianda48@gmail.com>
2023-05-26 11:45:00 +0700

    Update vim options

--- neovim/lua/options.lua
+++ neovim/lua/options.lua
@@ -3,32 +3,61 @@ vim.g.python3_host_prog = '/opt/homebrew/bin/python3'
 vim.g.loaded_perl_provider = 0
 
 vim.o.inccommand = 'nosplit'
-vim.o.hlsearch = true
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
 vim.o.hidden = true
+
+-- Enable mouse mode
 vim.o.mouse = 'a'
+
 vim.o.breakindent = true
+
 vim.o.ignorecase = true
+
 vim.o.smartcase = true
-vim.o.relativenumber = true
+
 vim.o.colorcolumn = '80'
+
 vim.o.signcolumn = 'yes'
+
+-- Decrease update time
 vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
 vim.o.guifont = 'Hack Nerd Font 12'
+
 vim.o.guicursor = ''
+
 vim.o.swapfile = false
+
 vim.o.backup = false
+
 vim.o.showmode = false
+
 vim.o.scrolloff = 8
+
 vim.o.cmdheight = 2
+
 vim.o.smartindent = true
+
 vim.o.errorbells = false
+
 vim.o.tabstop = 4
+
 vim.o.softtabstop = 4
+
 vim.o.shiftwidth = 4
+
 vim.o.completeopt = 'menuone,noselect'
 
+-- Make line numbers default
 vim.wo.number = true
+
 vim.wo.signcolumn = 'yes'
+
 vim.wo.wrap = false
 
 vim.bo.expandtab = true
