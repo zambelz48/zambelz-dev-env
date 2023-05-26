310c1caee53adc665f0e873462d81e2698d2d90b
zambelz48 <nanda.julianda48@gmail.com>
2023-05-26 11:37:46 +0700

    Update treesitter config

--- neovim/lua/treesitter_conf.lua
+++ neovim/lua/treesitter_conf.lua
@@ -1,20 +1,63 @@
 -- Parsers must be installed manually via :TSInstall (https://github.com/nvim-treesitter/nvim-treesitter#language-parsers)
 -- list of execite this => :TSInstall bash c cmake cpp css dockerfile html java javascript jsdoc json json5 jsonc kotlin llvm lua python ruby rust scss swift toml tsx typescript vim yaml
 require('nvim-treesitter.configs').setup {
-	highlight = {
-		enable = true, -- false will disable the whole extension
+	-- Add languages to be installed here that you want installed for treesitter
+	ensure_installed = {
+		'bash',
+		'c',
+		'cmake',
+		'cpp',
+		'css',
+		'dockerfile',
+		'git_config',
+		'git_rebase',
+		'gitattributes',
+		'gitcommit',
+		'gitignore',
+		'html',
+		'java',
+		'javascript',
+		'jsdoc',
+		'json',
+		'json5',
+		'jsonc',
+		'kotlin',
+		'llvm',
+		'lua',
+		'luadoc',
+		'make',
+		'markdown',
+		'mermaid',
+		'proto',
+		'python',
+		'query',
+		'regex',
+		'ruby',
+		'rust',
+		'scss',
+		'sql',
+		'swift',
+		'toml',
+		'tsx',
+		'typescript',
+		'vim',
+		'vimdoc',
+		'yaml'
 	},
+
+	-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+	auto_install = false,
+
+	highlight = { enable = true },
+	indent = { enable = true, disable = { 'python' } },
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			init_selection = 'gnn',
-			node_incremental = 'grn',
-			scope_incremental = 'grc',
-			node_decremental = 'grm',
+			init_selection = '<c-space>',
+			node_incremental = '<c-space>',
+			scope_incremental = '<c-s>',
+			node_decremental = '<M-space>',
 		},
-	},
-	indent = {
-		enable = true,
 	},
 	textobjects = {
 		select = {
@@ -22,6 +65,8 @@ require('nvim-treesitter.configs').setup {
 			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 			keymaps = {
 				-- You can use the capture groups defined in textobjects.scm
+				['aa'] = '@parameter.outer',
+				['ia'] = '@parameter.inner',
 				['af'] = '@function.outer',
 				['if'] = '@function.inner',
 				['ac'] = '@class.outer',
@@ -48,11 +93,20 @@ require('nvim-treesitter.configs').setup {
 				['[]'] = '@class.outer',
 			},
 		},
+		swap = {
+			enable = true,
+			swap_next = {
+				['<leader>a'] = '@parameter.inner',
+			},
+			swap_previous = {
+				['<leader>A'] = '@parameter.inner',
+			},
+		},
 	},
 	playground = {
 		enable = true,
 		disable = {},
-		updatetime = 25,     -- Debounced time for highlighting nodes in the playground from source code
+		updatetime = 25,   -- Debounced time for highlighting nodes in the playground from source code
 		persist_queries = false, -- Whether the query persists across vim sessions
 		keybindings = {
 			toggle_query_editor = 'o',
