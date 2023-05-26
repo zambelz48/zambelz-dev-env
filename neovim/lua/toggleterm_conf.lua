a7d118f3387e4f706226fa29fccdd6b8cea3edb3
zambelz48 <nanda.julianda48@gmail.com>
2023-05-26 08:27:22 +0700

    Remove unused command in toggleterm config

--- neovim/lua/toggleterm_conf.lua
+++ neovim/lua/toggleterm_conf.lua
@@ -5,7 +5,7 @@ require('toggleterm').setup({
 	open_mapping = [[<c-\>]],
 	hide_numbers = true,
 	shade_filetypes = {},
-	shade_terminals = true,
+	shade_terminals = false,
 	shading_factor = 2,
 	start_in_insert = true,
 	insert_mappings = true,
@@ -32,17 +32,5 @@ function _G.set_terminal_keymaps()
 	utils.tnoremap_buf('<C-l>', [[<C-\><C-n><C-W>l]])
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
-local Terminal = require("toggleterm.terminal").Terminal
-
-local repl_node = Terminal:new({ cmd = "node", hidden = true })
-function _REPL_NODE()
-	repl_node:toggle()
-end
-
-local repl_swift = Terminal:new({ cmd = "swift repl", hidden = true })
-function _REPL_SWIFT()
-	repl_swift:toggle()
-end
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
 
