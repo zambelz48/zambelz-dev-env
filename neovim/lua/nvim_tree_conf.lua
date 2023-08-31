6c5c398df4286d5e22c577ee4592a6df0abc01ab
zambelz48 <nanda.julianda48@gmail.com>
2023-08-31 10:22:07 +0700

    Remove view.mappings config

--- neovim/lua/nvim_tree_conf.lua
+++ neovim/lua/nvim_tree_conf.lua
@@ -49,14 +49,7 @@ require'nvim-tree'.setup {
 		-- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
 		width = 60,
 		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-		side = 'left',
-		mappings = {
-			-- custom only false will merge the list with the default mappings
-			-- if true, it will only use your list to set the mappings
-			custom_only = false,
-			-- list of mappings to set on the tree manually
-			list = {}
-		}
+		side = 'left'
 	},
 
 	git = {
