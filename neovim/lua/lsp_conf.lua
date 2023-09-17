26748b7b38e65fb65272aa6977692fd86bc12c3a
Nanda Julianda Akbar <nanda.julianda48@gmail.com>
2023-09-17 17:30:26 +0700

    Adjust compose lsp config

--- neovim/lua/lsp_conf.lua
+++ neovim/lua/lsp_conf.lua
@@ -171,7 +171,7 @@ local servers = {
 	{
 		name = 'docker_compose_language_service',
 		cmd = { 'docker-compose-langserver', '--stdio' },
-		filetypes = { 'yaml' },
+		filetypes = { 'yaml.docker-compose' },
 		single_file_support = true,
 		root_dir = nvim_lsp.util.root_pattern('docker-compose.yaml')
 	},
