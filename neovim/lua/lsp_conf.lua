8b7e4a2904bca198b36d5bfc4a2a0b73b903b5c0
Nanda Julianda Akbar <nanda.julianda48@gmail.com>
2023-09-17 17:07:17 +0700

    Adjust html lsp config

--- neovim/lua/lsp_conf.lua
+++ neovim/lua/lsp_conf.lua
@@ -72,6 +72,15 @@ local servers = {
 		name = 'html',
 		cmd = { 'vscode-html-language-server', '--stdio' },
 		filetypes = { 'html' },
+		single_file_support = true,
+		init_options = {
+			configurationSection = { "html", "css", "javascript" },
+			embeddedLanguages = {
+				css = true,
+				javascript = true
+			},
+			provideFormatter = true
+		}
 	},
 	{
 		name = 'lua_ls',
