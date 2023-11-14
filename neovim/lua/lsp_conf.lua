512079984a8c099f8f7935e71f7d666d711c21ea
zambelz48 <nanda.julianda48@gmail.com>
2023-11-14 15:18:15 +0700

    Update eslint lsp config

--- neovim/lua/lsp_conf.lua
+++ neovim/lua/lsp_conf.lua
@@ -96,6 +96,38 @@ local servers = {
 			'vue',
 			'svelte'
 		},
+		settings = {
+			codeAction = {
+				disableRuleComment = {
+					enable = true,
+					location = "separateLine"
+				},
+				showDocumentation = {
+					enable = true
+				}
+			},
+			codeActionOnSave = {
+				enable = false,
+				mode = "all"
+			},
+			experimental = {
+				useFlatConfig = false
+			},
+			format = true,
+			nodePath = "",
+			onIgnoredFiles = "off",
+			problems = {
+				shortenToSingleLine = false
+			},
+			quiet = false,
+			rulesCustomizations = {},
+			run = "onType",
+			useESLintClass = false,
+			validate = "on",
+			workingDirectory = {
+				mode = "location"
+			}
+		}
 	},
 	{
 		name = 'html',
