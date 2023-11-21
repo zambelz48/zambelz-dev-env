43e3f9d9416465b9354b1800683028c466e50254
zambelz48 <nanda.julianda48@gmail.com>
2023-11-21 16:47:25 +0700

    Add neovim keymaps

--- neovim/lua/keymaps.lua
+++ neovim/lua/keymaps.lua
@@ -1,7 +1,7 @@
 
 local utils = require('utils')
 
--- keymap
+-- fugitive keymaps
 utils.nnoremap('<leader>gs', ':G<CR>', { silent = true })
 utils.nnoremap('<leader>gl', ':G log<CR>', { silent = true })
 utils.nnoremap('<leader>glo', ':G log --oneline<CR>', { silent = true })
@@ -9,4 +9,7 @@ utils.nnoremap('<leader>ga', ':Gwrite<CR>', { silent = true })
 utils.nnoremap('gds', ':Gdiffsplit<CR>', { silent = true })
 utils.nnoremap('<leader>gp', ':G push<CR>', { silent = true })
 utils.nnoremap('<leader>gpo', ':G push origin ')
+
+-- eslint keymaps
+utils.nnoremap('<leader>EF', ':EslintFixAll<CR>', { silent = true })
 
