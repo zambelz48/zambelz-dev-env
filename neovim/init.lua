3349772a3e6fd90ad87ca25ead91bac87d13695c
zambelz48 <nanda.julianda48@gmail.com>
2024-04-10 00:02:59 +0700

    initial add nvim-treesitter-context

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,104 +1,106 @@
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 vim.api.nvim_exec(
-	[[
+    [[
         augroup Packer
             autocmd!
             autocmd BufWritePost init.lua PackerCompile
         augroup end
     ]],
-	false
+    false
 )
 
 local use = require('packer').use
 require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use 'nvim-tree/nvim-web-devicons'
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = 'nvim-tree/nvim-web-devicons'
-	}
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = {
-			'nvim-tree/nvim-web-devicons',
-			opt = true
-		}
-	}
-	use 'tpope/vim-fugitive'
-	use 'nvim-lua/plenary.nvim'
-	use 'ibhagwan/fzf-lua'
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = { 'nvim-lua/plenary.nvim' }
-	}
-	use {
-		'nvim-telescope/telescope-fzf-native.nvim',
-		run =
-		'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-	}
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		requires = { 'nvim-treesitter/nvim-treesitter-textobjects' }
-	}
-	use 'nvim-treesitter/playground'
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'saadparwaiz1/cmp_luasnip'
-	use {
-		'L3MON4D3/LuaSnip',
-		run = "make install_jsregexp"
-	}
-	use {
-		'dracula/vim',
-		as = 'dracula'
-	}
-	use 'edkolev/tmuxline.vim'
-	use 'norcalli/nvim-colorizer.lua'
-	use {
-		'iamcco/markdown-preview.nvim',
-		run = function() vim.fn["mkdp#util#install"]() end
-	}
-	use {
-		'akinsho/toggleterm.nvim',
-		tag = '*'
-	}
-	use 'lewis6991/gitsigns.nvim'
-	use {
-		'folke/trouble.nvim',
-		requires = 'nvim-tree/nvim-web-devicons'
-	}
-	use {
-		'j-hui/fidget.nvim'
-	}
-	use 'numToStr/Comment.nvim'
-	use 'JoosepAlviste/nvim-ts-context-commentstring'
-	use 'mfussenegger/nvim-jdtls'
-	use 'mfussenegger/nvim-dap'
-	use 'nvim-neotest/nvim-nio'
-	use 'rcarriga/nvim-dap-ui'
-	use 'theHamsta/nvim-dap-virtual-text'
-	use 'tpope/vim-dadbod'
-	use 'kristijanhusak/vim-dadbod-ui'
-	use 'kristijanhusak/vim-dadbod-completion'
-	use 'zbirenbaum/copilot.lua'
-	use 'AndreM222/copilot-lualine'
-	use {
-		'CopilotC-Nvim/CopilotChat.nvim',
-		branch = 'canary',
-	}
-	use "pmizio/typescript-tools.nvim"
+    use 'wbthomason/packer.nvim'
+    use 'nvim-tree/nvim-web-devicons'
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {
+            'nvim-tree/nvim-web-devicons',
+            opt = true
+        }
+    }
+    use 'tpope/vim-fugitive'
+    use 'nvim-lua/plenary.nvim'
+    use 'ibhagwan/fzf-lua'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run =
+        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        requires = { 'nvim-treesitter/nvim-treesitter-textobjects' }
+    }
+    use 'nvim-treesitter/nvim-treesitter-context'
+    use 'nvim-treesitter/playground'
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'saadparwaiz1/cmp_luasnip'
+    use {
+        'L3MON4D3/LuaSnip',
+        run = "make install_jsregexp"
+    }
+    use {
+        'dracula/vim',
+        as = 'dracula'
+    }
+    use 'edkolev/tmuxline.vim'
+    use 'norcalli/nvim-colorizer.lua'
+    use {
+        'iamcco/markdown-preview.nvim',
+        run = function() vim.fn["mkdp#util#install"]() end
+    }
+    use {
+        'akinsho/toggleterm.nvim',
+        tag = '*'
+    }
+    use 'lewis6991/gitsigns.nvim'
+    use {
+        'folke/trouble.nvim',
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
+    use {
+        'j-hui/fidget.nvim'
+    }
+    use 'numToStr/Comment.nvim'
+    use 'JoosepAlviste/nvim-ts-context-commentstring'
+    use 'mfussenegger/nvim-jdtls'
+    use 'mfussenegger/nvim-dap'
+    use 'nvim-neotest/nvim-nio'
+    use 'rcarriga/nvim-dap-ui'
+    use 'theHamsta/nvim-dap-virtual-text'
+    use 'tpope/vim-dadbod'
+    use 'kristijanhusak/vim-dadbod-ui'
+    use 'kristijanhusak/vim-dadbod-completion'
+    use 'zbirenbaum/copilot.lua'
+    use 'AndreM222/copilot-lualine'
+    use {
+        'CopilotC-Nvim/CopilotChat.nvim',
+        branch = 'canary',
+    }
+    use "pmizio/typescript-tools.nvim"
     use "folke/todo-comments.nvim"
 end)
 
 require('options')
 require('mappings')
 require('treesitter_conf')
+require('treesitter_context_conf')
 require('lsp_conf')
 require('cmp_conf')
 require('typescript_tools_conf')
