ce0fe2344e4797e1c417b1eaa4055cbb1ee30cba
Nanda Julianda Akbar <nanda.julianda48@gmail.com>
2024-08-19 11:56:41 +0700

    update telescope keymaps

--- neovim/lua/telescope_conf.lua
+++ neovim/lua/telescope_conf.lua
@@ -15,14 +15,29 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- Keymaps
 vim.keymap.set('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = true})<CR>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = true})<CR>]], { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>sfc', ':Telescope find_files search_dirs=', { noremap = true })
+
+vim.keymap.set('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>sdi', function()
+    require('telescope.builtin').grep_string({
+        search = vim.fn.input('Find word: '),
+        only_sort_text = true,
+    })
+end, { noremap = true })
+
+vim.keymap.set('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
@@ -31,7 +46,3 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
-
--- Additional keymaps
--- Find files in specific directory
-vim.keymap.set('n', '<leader>sfc', ':Telescope find_files search_dirs=', { noremap = true })
