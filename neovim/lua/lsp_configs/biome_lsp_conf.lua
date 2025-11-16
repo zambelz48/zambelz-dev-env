eaa51c9bc4817c278c2ad139eb6e8a5836423945
zambelz48 <nanda.julianda48@gmail.com>
2025-11-17 01:48:56 +0700

    fix biome lsp

--- neovim/lua/lsp_configs/biome_lsp_conf.lua
+++ neovim/lua/lsp_configs/biome_lsp_conf.lua
@@ -1,5 +1,4 @@
 return {
     name = 'biome',
-    cmd = { 'biome' },
     filetypes = { 'astro', 'css', 'graphql', 'html', 'javascript', 'javascriptreact', 'json', 'jsonc', 'svelte', 'typescript', 'typescript.tsx', 'typescriptreact', 'vue' },
 }
