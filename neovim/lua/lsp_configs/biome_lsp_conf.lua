d54ccfa602f90521bdab811689feb093096a0d57
zambelz48 <nanda.julianda48@gmail.com>
2025-11-17 00:51:52 +0700

    initial add biomejs neovim lsp config

---
+++ neovim/lua/lsp_configs/biome_lsp_conf.lua
@@ -0,0 +1,5 @@
+return {
+    name = 'biome',
+    cmd = { 'biome' },
+    filetypes = { 'astro', 'css', 'graphql', 'html', 'javascript', 'javascriptreact', 'json', 'jsonc', 'svelte', 'typescript', 'typescript.tsx', 'typescriptreact', 'vue' },
+}
